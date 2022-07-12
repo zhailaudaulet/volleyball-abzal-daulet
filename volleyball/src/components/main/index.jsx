9b4d8171045cf094e512dec1ed6628950dafa288
Daulet <daulet.ilchanov@gmail.com>
2022-07-12 21:01:12 +0600

    main ready

--- volleyball/src/components/main/index.jsx
+++ volleyball/src/components/main/index.jsx
@@ -14,6 +14,28 @@ export const Main = () => {
                 <div className='jumpHeight'>
                     <input className='commonInput' placeholder='Optional' />
                 </div>
+                <div className='sjom'>
+                    <input className='commonInput' placeholder='Optional' />
+                </div>
+                <div className='age'>
+                    <input className='commonInput' placeholder='Optional' />
+                </div>
+                <div className='position'>
+                    <select className='commonInput' name='optional'>
+                        <option disabled selected>Optional</option>
+                        <option>Outside Hitter</option>
+                        <option>Setter</option>
+                        <option>Pinch Server</option>
+                        <option>Opposite Hitter</option>
+                        <option>Middle Blocker</option>
+                        <option>Libero</option>
+                        <option >Ace</option>
+                        <option>Captain</option>
+                    </select>
+                </div>
+                <div className='goButton'>
+                    <p className='goText'>GO</p>
+                </div>
 
 
 
