804efd7d624308693200fbf24ba6234e43295a32
Daulet <daulet.ilchanov@gmail.com>
2022-07-13 01:30:04 +0600

    7 krugov ada the end almos

--- volleyball/src/components/result/index.jsx
+++ volleyball/src/components/result/index.jsx
@@ -9,11 +9,49 @@ export const Result = () => {
                 <div className='characterImg'>
                     Some Image
                 </div>
-                <div className='commonStatsBar'>
+                <div className='notCommonStatsBar'>
+                    <p className='teeext'>Height</p>
                     <progress max={100} value={75} className='characterStats'>
                     </progress>
                     <progress max={100} value={70} className='yourStats'></progress>
                 </div>
+
+                <div className='notCommonStatsBar2'>
+                    <progress max={100} value={75} className='characterStats'>
+                    </progress>
+                    <progress max={100} value={70} className='yourStats'></progress>
+                </div>
+
+                <div className='notCommonStatsBar3'>
+                    <progress max={100} value={75} className='characterStats'>
+                    </progress>
+                    <progress max={100} value={70} className='yourStats'></progress>
+                </div>
+
+                <div className='notCommonStatsBar4'>
+                    <progress max={100} value={75} className='characterStats'>
+                    </progress>
+                    <progress max={100} value={70} className='yourStats'></progress>
+                </div>
+
+                <div className='notCommonStatsBar5'>
+                    <progress max={100} value={75} className='characterStats'>
+                    </progress>
+                    <progress max={100} value={70} className='yourStats'></progress>
+                </div>
+
+                <div className='notCommonStatsBar6'>
+                    <progress max={100} value={75} className='characterStats'>26
+                    </progress>
+                    
+                </div>
+                <div className='nameBox'><p className='charName'>Daulet Zhailau</p></div>
+                
+                <div className='basicInfo'>
+                    <p className='infoText'>Basic Info</p>
+                </div>
+
+
 
             </div>
 
