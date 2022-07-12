04c04ae97a85ba137d28c068deb6e66ba6abcb75
guinnod <210103064@stu.sdu.edu.kz>
2022-07-12 21:38:53 +0600

    get user data

--- volleyball/src/components/main/index.jsx
+++ volleyball/src/components/main/index.jsx
@@ -1,5 +1,25 @@
 import './index.css';
+import aobaJohsai from "../../data/aobaJohsai.json";
+import dateTech from "../../data/dateTech.json";
+import fukurodani from "../../data/fukurodani.json";
+import inarizaki from "../../data/inarizaki.json";
+import karasuno from "../../data/karasuno.json";
+import nekoma from "../../data/nekoma.json";
+import others from "../../data/others.json";
+import shiratorizawa from "../../data/shiratorizawa.json";
 export const Main = () => {
+    const defineChar = () => {
+
+        const dataChar = [...aobaJohsai, ...dateTech, ...fukurodani, ...inarizaki, ...karasuno, ...nekoma, ...others, ...shiratorizawa];
+
+        const dataUser = [];
+
+        for (let index = 0; index < 6; index++) {
+            dataUser[index] = document.getElementById(`${index}`).value;
+        }
+
+
+    }
     return (
 
         <div className="mainBody">
@@ -7,27 +27,27 @@ export const Main = () => {
             <div className='pole'>
                 <div className='height'>
                     <p className='commonText'>Height</p>
-                    <input className='commonInput' placeholder='Optional' />
+                    <input className='commonInput' placeholder='Optional' id='0' />
                 </div>
                 <div className='weight'>
                     <p className='commonText'>Weight</p>
-                    <input className='commonInput' placeholder='Optional' />
+                    <input className='commonInput' placeholder='Optional' id='1' />
                 </div>
                 <div className='jumpHeight'>
                     <p className='commonText'>Jumping Reach</p>
-                    <input className='commonInput' placeholder='Optional' />
+                    <input className='commonInput' placeholder='Optional' id='2' />
                 </div>
                 <div className='sjom'>
                     <p className='commonText'>Spike</p>
-                    <input className='commonInput' placeholder='Optional' />
+                    <input className='commonInput' placeholder='Optional' id='3' />
                 </div>
                 <div className='age'>
                     <p className='commonText'>Age</p>
-                    <input className='commonInput' placeholder='Optional' />
+                    <input className='commonInput' placeholder='Optional' id='4' />
                 </div>
                 <div className='position'>
                     <p className='commonText'>Position</p>
-                    <select className='commonInput' name='optional'>
+                    <select className='commonInput' name='optional' id='5'>
                         <option disabled selected >Optional</option>
                         <option>Outside Hitter</option>
                         <option>Setter</option>
@@ -39,12 +59,9 @@ export const Main = () => {
                         <option>Captain</option>
                     </select>
                 </div>
-                <div className='goButton'>
+                <div className='goButton' onClick={defineChar}>
                     <p className='goText'>GO</p>
                 </div>
-
-
-
             </div>
 
         </div>
