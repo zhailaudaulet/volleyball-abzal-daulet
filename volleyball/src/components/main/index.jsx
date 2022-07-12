85dacc578b4cf1e990ac198d519b01e756c0b939
Daulet <daulet.ilchanov@gmail.com>
2022-07-12 20:26:11 +0600

    abzal poprosil

--- volleyball/src/components/main/index.jsx
+++ volleyball/src/components/main/index.jsx
@@ -1,3 +1,22 @@
+import './index.css';
 export const Main = () => {
-    return (<div>Main</div>);
+    return (
+
+        <div className="mainBody">
+
+            <div className='pole'>
+                <div className='height'>
+                    <input className='commonInput' placeholder='Optional' />
+                </div>
+                <div className='weight'>
+                    <input className='commonInput' placeholder='Optional' />
+                </div>
+                
+
+
+            </div>
+
+        </div>
+
+    );
 }
